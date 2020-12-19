f36601c3997609b83bc1d4f3861da5a910418710
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-19 12:08:49 +0200

    init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemosTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemosTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59562de3-1d5f-4d0a-917e-b9eb20ea4955")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;  //MSTest Framework -supports unitTesting in Visual Studio
+using System;
+
+namespace DemosTests
+{
+    [TestClass]   // says that this class will be unit Test// this class will be unit Test Class !
+    public class UnitTest1
+    {
+        private void SetUp() 
+        {
+          
+        }
+
+        [TestMethod]  // this method will be unit method test, this method will be a unit Test
+       // [ExpectedException(typeof(DivideByZeroException))]
+        public void Test_Demo ()
+        {
+            var zero = 0;
+            var result = 1 / zero;
+            // if expected result === actual result then the Test is passed, 
+            // if the expected result != actual result after perforaming the code the Test i failed !
+        }
+    }
+}
